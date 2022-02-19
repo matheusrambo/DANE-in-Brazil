adeaec5ee982c60fe75657c91f5c1b607a566ff9
Matheus Rambo da Roza <36770574+matheusrambo@users.noreply.github.com>
2022-02-19 10:22:50 -0300

    Code to convert
    convert crawler DNS output to crawler SMTP input.

---
+++ DNS-to-SMTP/DNS-to-TLSA.py
@@ -0,0 +1,38 @@
+import json
+import time
+filename = time.strftime("%Y-%m-%d")
+output = open('input.txt', 'w+')
+with open('entrada/' + filename + '-dns.json') as json_file:
+    entrada = json_file.readlines()
+
+for i in range (len(entrada)):
+    data = (json.loads(entrada[i]))
+    cont = 0
+    for i in range(0,10): #contar quantos MXs possue em tal dominio
+        try:
+            if (data['results']['MAIL'][i]['host']) != "":
+                cont = cont + 1
+        except:
+            cont = cont
+    for i in range (0,cont):
+        porta = "25"
+        try:
+            mx = data['results']['MAIL'][i]['host']
+            usage = data['results']['MAIL'][i]['TLSA']['25'][0]['usage']
+            selector = data['results']['MAIL'][i]['TLSA']['25'][0]['selector']
+            matchingtype = data['results']['MAIL'][i]['TLSA']['25'][0]['matchingtype']
+            data_key = data['results']['MAIL'][i]['TLSA']['25'][0]['data']
+        except:
+            usage = 0
+            selector = 0
+            matchingtype = 0
+            data_key = 0
+        if usage==0 or selector==0 or matchingtype==0 or data_key==0:
+            pass
+        else:   
+             
+            linha = "_" + porta + "._tcp." + mx[:-1] + "\n"
+            print(linha)
+            output.write(linha)
+
+
